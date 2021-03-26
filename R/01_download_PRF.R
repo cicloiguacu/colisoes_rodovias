b569e18f3b9553357e4f4325ccbe0fb245d12040
Joao Bazzo <joao.bazzo@gmail.com>
2021-03-26 15:35:51 -0300

    first codes

---
+++ R/01_download_PRF.R
@@ -0,0 +1,70 @@
+#
+#
+# download dado PRF
+#
+#
+library(data.table)
+library(magrittr)
+
+link <- "http://arquivos.prf.gov.br/arquivos/index.php/s/jdDLrQIf33xXSCe/download"
+dir.create("data")
+dir.create("data-raw/")
+
+
+# Agrupados por ocorrência
+#
+download.file(url = "http://arquivos.prf.gov.br/arquivos/index.php/s/jdDLrQIf33xXSCe/download",destfile = "data-raw/2020.zip")
+download.file(url = "http://arquivos.prf.gov.br/arquivos/index.php/s/kRBUylqz6DyQznN/download",destfile = "data-raw/2019.zip")
+download.file(url = "http://arquivos.prf.gov.br/arquivos/index.php/s/MaC6cieXSFACNWT/download",destfile = "data-raw/2018.zip")
+download.file(url = "http://arquivos.prf.gov.br/arquivos/index.php/s/nqvFu7xEF6HhbAq/download",destfile = "data-raw/2017.zip")
+download.file(url = "http://arquivos.prf.gov.br/arquivos/index.php/s/AhSKXYgrFtfXMK3/download",destfile = "data-raw/2016.rar")
+download.file(url = "http://arquivos.prf.gov.br/arquivos/index.php/s/qqAsQep7J8FzpR5/download",destfile = "data-raw/2015.rar")
+download.file(url = "http://arquivos.prf.gov.br/arquivos/index.php/s/1QYIZKqjcUDOrXm/download",destfile = "data-raw/2014.rar")
+download.file(url = "http://arquivos.prf.gov.br/arquivos/index.php/s/ZBJgHd4fmYV2Hhr/download",destfile = "data-raw/2013.rar")
+download.file(url = "http://arquivos.prf.gov.br/arquivos/index.php/s/VIeiSbpxRxan33L/download",destfile = "data-raw/2012.rar")
+download.file(url = "http://arquivos.prf.gov.br/arquivos/index.php/s/mBWHzsujvZ7nZbe/download",destfile = "data-raw/2011.rar")
+download.file(url = "http://arquivos.prf.gov.br/arquivos/index.php/s/tx9oSTOPYrSqDhb/download",destfile = "data-raw/2010.rar")
+download.file(url = "http://arquivos.prf.gov.br/arquivos/index.php/s/QZXR9LVr4lynqbA/download",destfile = "data-raw/2009.rar")
+download.file(url = "http://arquivos.prf.gov.br/arquivos/index.php/s/WEP8Pu4sA64V7f6/download",destfile = "data-raw/2008.rar")
+download.file(url = "http://arquivos.prf.gov.br/arquivos/index.php/s/EzXry9IsLLJosSg/download",destfile = "data-raw/2007.rar")
+
+# Agrupados por pessoa
+download.file(url = "http://arquivos.prf.gov.br/arquivos/index.php/s/35cUcYXWsrJd4tF/download",destfile = "data-raw/pessoa_2020.zip")
+download.file(url = "http://arquivos.prf.gov.br/arquivos/index.php/s/vw74viLA7WuZI4H/download",destfile = "data-raw/pessoa_2019.zip")
+download.file(url = "http://arquivos.prf.gov.br/arquivos/index.php/s/MJRqaDMQ27F60Ml/download",destfile = "data-raw/pessoa_2018.zip")
+download.file(url = "http://arquivos.prf.gov.br/arquivos/index.php/s/PYMCvPsVa3ZHspf/download",destfile = "data-raw/pessoa_2017.rar")
+download.file(url = "http://arquivos.prf.gov.br/arquivos/index.php/s/xhayuB1bJhwapp7/download",destfile = "data-raw/pessoa_2016.rar")
+download.file(url = "http://arquivos.prf.gov.br/arquivos/index.php/s/hwLX6qhOh3PeYCD/download",destfile = "data-raw/pessoa_2015.rar")
+download.file(url = "http://arquivos.prf.gov.br/arquivos/index.php/s/SX9OjkKdermNOz2/download",destfile = "data-raw/pessoa_2014.rar")
+download.file(url = "http://arquivos.prf.gov.br/arquivos/index.php/s/IKvS9Ct4HrETjQV/download",destfile = "data-raw/pessoa_2013.rar")
+download.file(url = "http://arquivos.prf.gov.br/arquivos/index.php/s/SB4cbUhtNIwQ3np/download",destfile = "data-raw/pessoa_2012.rar")
+download.file(url = "http://arquivos.prf.gov.br/arquivos/index.php/s/2IWdQpI7EVj90DG/download",destfile = "data-raw/pessoa_2011.rar")
+download.file(url = "http://arquivos.prf.gov.br/arquivos/index.php/s/SUChWEElsoC1h8h/download",destfile = "data-raw/pessoa_2010.rar")
+download.file(url = "http://arquivos.prf.gov.br/arquivos/index.php/s/za17hh8I0jY9yGi/download",destfile = "data-raw/pessoa_2009.rar")
+download.file(url = "http://arquivos.prf.gov.br/arquivos/index.php/s/O805ZiovElHiwuY/download",destfile = "data-raw/pessoa_2008.rar")
+download.file(url = "http://arquivos.prf.gov.br/arquivos/index.php/s/2chKvzigLF9iZhy/download",destfile = "data-raw/pessoa_2007.rar")
+
+# Agrupados por pessoa - Todas as causas e tipos de acidentes (a partir de 2017)
+download.file(url = "http://arquivos.prf.gov.br/arquivos/index.php/s/sdvJndbl5wLyh3J/download",destfile = "data-raw/agrupado_2019.rar")
+download.file(url = "http://arquivos.prf.gov.br/arquivos/index.php/s/EF4uPKCihT0ouXd/download",destfile = "data-raw/agrupado_2018.rar")
+download.file(url = "http://arquivos.prf.gov.br/arquivos/index.php/s/kgJ0ea8QZrix5Yt/download",destfile = "data-raw/agrupado_2017.rar")
+#download.file(url = "",destfile = "data-raw/agrupado_2019.rar")
+
+# extract files
+zip_files <- list.files(path = "data-raw",pattern = "agrupado",full.names = TRUE)
+zip_files
+# zip_file_names <- list.files(path = "data-raw/",full.names = FALSE)
+# zip_file_names <- gsub(".zip","",zip_file_names)
+# zip_file_names <- gsub(".rar","",zip_file_names)
+# zip_file_names
+
+for(i in seq_along(zip_files)){ # i = 25
+
+  message(zip_files[i])
+  if(zip_files[i] %like% ".rar"){
+    archive_extract(archive = zip_files[i],dir =  "data/")
+  }else{
+    utils::unzip(zipfile = zip_files[i],exdir = "data/")
+  }
+
+}
---
+++ R/02_read_PRF.R
@@ -0,0 +1,150 @@
+rm(list=ls())
+
+library(data.table)
+library(magrittr)
+library(mapview)
+library(ggplot2)
+library(stringr)
+`%nin%` = Negate(`%in%`)
+`%nlike%` = Negate(`%like%`)
+
+### read files - vitima
+#
+
+list_years_acidentes <- list.files("data",pattern = "acidentes",full.names = "TRUE")
+list_years_acidentes <- list_years_acidentes[list_years_acidentes %nlike% "causa"]
+
+base_id_veiculo <- lapply(seq_along(list_years_acidentes),function(i){
+  tmp_fleet <- data.table::fread(list_years_acidentes[i],encoding = "Latin-1")
+  tmp_fleet <- tmp_fleet[id_veiculo != "(null)" & tipo_veiculo != "(null)"]
+  tmp_fleet <- tmp_fleet[,.SD[1],by = .(id_veiculo,tipo_veiculo,marca)][,.(id_veiculo,tipo_veiculo,marca)]
+  return(tmp_fleet)
+}) %>% data.table::rbindlist()
+base_id_veiculo <- base_id_veiculo[,.SD[1],by = .(id_veiculo)]
+
+vitima_all <- lapply(seq_along(list_years_acidentes),function(i){ # i = 1
+
+  # i = 12
+  # initial message
+  message("-------------------------------------")
+  message(list_years_acidentes[i])
+  message("-------------------------------------")
+
+  # read
+  tmp_file <- data.table::fread(list_years_acidentes[i],encoding = "Latin-1")
+  tmp_file <- tmp_file[uf == "PR",]
+
+  # adjust date
+  tmp_file[,ano := data.table::as.IDate(data_inversa, format = "%d/%m/%Y") %>% data.table::year()]
+  tmp_file[,ano := data.table::fifelse(ano == 16,2016,ano)]
+
+  # estado fisico da vitima
+
+  tmp_file[,estado_fisico := stringi::stri_trans_general(str = estado_fisico,id = "Latin-ASCII") %>% tolower()]
+
+  if(tmp_file$ano[1] %in% c(2017,2018,2019,2020)){
+    tmp_file[feridos_graves == 1,estado_fisico := "ferido grave"]
+    tmp_file[feridos_leves == 1,estado_fisico := "ferido leve"]
+    tmp_file[mortos == 1,estado_fisico := "morto"]
+    tmp_file[ilesos == 1,estado_fisico := "ileso"]
+    tmp_file[estado_fisico == "nao informado", estado_fisico := "nao_informado"]
+  }else{
+
+    tmp_file[estado_fisico %like% "ferido grave" , estado_fisico := "ferido grave"]
+    tmp_file[estado_fisico %like% "ferido leve" , estado_fisico := "ferido leve"]
+    tmp_file[estado_fisico %like% "morto" , estado_fisico := "morto"]
+    tmp_file[estado_fisico %like% "ileso" , estado_fisico := "ileso"]
+    tmp_file[estado_fisico %like% "ignorado" |
+               estado_fisico %like% "(null)", estado_fisico := "nao_informado"]
+
+    # por classificacao de acidente
+    tmp_file[classificacao_acidente %like% "Sem Vítimas" & estado_fisico == "nao_informado",
+             estado_fisico := "ileso"]
+
+  }
+
+
+  tmp_file$estado_fisico %>% base::table(useNA = "always")
+
+  # remove testemunhas
+  tmp_file <- tmp_file[tipo_envolvido %nin% "Testemunha"]
+
+  # classificacao veiculo
+  # add missing fleet data
+  if(nrow(tmp_file[tipo_veiculo == "(null)"]) > 0){
+
+    message("completando com base externa de frota")
+
+
+    id_to_fix <- c(which(tmp_file$tipo_veiculo == "(null)"), which(tmp_file$id_veiculo != "(null)"))
+    id_to_fix <- id_to_fix[duplicated(id_to_fix)]
+    # add base
+    tmp_file[base_id_veiculo,on = "id_veiculo",tipo_veiculo_base := i.tipo_veiculo]
+    tmp_file[id_to_fix, tipo_veiculo := tipo_veiculo_base]
+    tmp_file[,tipo_veiculo_base := NULL]
+  }
+
+  # to lower
+  tmp_file[,tipo_veiculo := stringi::stri_trans_general(str = tipo_veiculo,id = "Latin-ASCII") %>% tolower()]
+  tmp_file[,tipo_envolvido := stringi::stri_trans_general(str = tipo_envolvido,id = "Latin-ASCII") %>% tolower()]
+
+  tmp_file$tipo_veiculo %>% base::table(useNA = "always")
+  tmp_file$tipo_envolvido %>% base::table(useNA = "always")
+
+  tmp_file[tipo_envolvido %in% "pedestre" , tipo_veiculo := "pedestre"]
+  tmp_file[tipo_veiculo %in% "pedestre" , classe_veiculo := "pedestre"]
+  tmp_file[tipo_veiculo %in% c("bicicleta","ciclomotor"),classe_veiculo := "bicicleta"]
+  tmp_file[tipo_veiculo %in% c("motocicletas","motocicleta","triciclo",
+                               "motoneta","quadriciclo"),classe_veiculo := "motocicleta"]
+  tmp_file[tipo_veiculo %in% c("automovel","caminhonete",
+                               "camioneta"),classe_veiculo := "veiculos_leves"]
+  tmp_file[tipo_veiculo %in% c("microonibus","micro-onibus",
+                               "utilitario","reboque"),classe_veiculo := "veiculo_medio_porte"]
+  tmp_file[tipo_veiculo %in% c("caminhao","caminhao-trator","semi-reboque","semireboque",
+                               "bonde / trem","trem-bonde","caminhao-tanque","onibus",
+                               "trator de rodas","trator misto","trator de esteira",
+                               "trator de esteiras"), classe_veiculo := "veiculo_grande_porte"]
+  tmp_file[tipo_veiculo %in% c("carroca","charrete",
+                               "carroca-charrete"),classe_veiculo := "veiculo_grande_porte"]
+  tmp_file[tipo_veiculo == "chassi-plataforma", classe_veiculo := "nao identificado"]
+  tmp_file[tipo_veiculo == "nao identificado" |
+             tipo_veiculo == "outros" |
+             tipo_veiculo == "nao informado", classe_veiculo := "nao identificado"]
+
+  tmp_file$classe_veiculo %>% base::table(useNA = "always")
+
+  tmp_file[is.na(classe_veiculo)]
+
+  # classificacao tipo de vitima conforme veiculo do ocupante
+
+  tmp_file$tipo_envolvido %>% base::table(useNA = "always")
+
+  tmp_file[ tipo_veiculo %in% "pedestre" |
+              tipo_veiculo %in% "carro-de-mao" |
+              tipo_veiculo %in% "carro de mao",tipo_vitima := "Pedestre"]
+  tmp_file[classe_veiculo %like% "motocicleta",tipo_vitima := "Ocupante motocicleta"]
+  tmp_file[classe_veiculo %like% "veiculos_leves",tipo_vitima := "Ocupante veic. pequeno porte"]
+  tmp_file[classe_veiculo %like% "veiculo_medio_porte",tipo_vitima := "Ocupante veic. médio porte"]
+  tmp_file[classe_veiculo %like% "veiculo_grande_porte",tipo_vitima := "Ocupante veic. grande porte"]
+  tmp_file[classe_veiculo %like% "bicicleta",tipo_vitima := "Ocupante bicicleta"]
+  tmp_file[tipo_envolvido %like% "cavaleiro",tipo_vitima := "Ocupante cavalo"]
+  tmp_file[classe_veiculo %like% "nao identificado",tipo_vitima := "Não identificado"]
+
+  tmp_file[is.na(tipo_vitima)]
+
+  # checagem
+  if(nrow(tmp_file[is.na(tipo_vitima)]) > 0){
+    message(paste0("check file ",list_years_acidentes[i]))
+    message(paste0("tmp_file[is.na(tipo_vitima)] = "),nrow(tmp_file[is.na(tipo_vitima)]))
+    tmp_file[is.na(tipo_vitima) ,tipo_vitima := "Não identificado"]
+  }
+
+  tmp_file$tipo_vitima %>% table()
+  # summary
+  tmp_file <- tmp_file[,.N,by = .(br,km,tipo_vitima,ano,estado_fisico)]
+
+  return(tmp_file)
+}) %>% data.table::rbindlist()
+
+
+readr::write_rds(vitima_all,"data/vitima_all.rds",compress = "gz")
