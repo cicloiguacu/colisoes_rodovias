e0b0563c94db28673b020b4fb6fb9c6e3fe642f6
Joao Bazzo <joao.bazzo@gmail.com>
2021-05-13 14:21:23 -0300

    atualiza memoria de calculo

--- R/05_memoria_de_calculo.R
+++ R/05_memoria_de_calculo.R
@@ -14,7 +14,20 @@ library(readr)
 
 
 vitima_all <- readr::read_rds("data/vitima_all.rds")
-
+fwrite(vitima_all,"data/vitima_all.csv")
+vitima_all[1]
+# custo por vitima
+# fonte: https://www.ipea.gov.br/portal/images/stories/PDFs/TDs/td_2565.pdf
+custo_vitima <- vitima_all[,sum(N),by = .(estado_fisico,ano)]
+custo_vitima[estado_fisico == "ileso",total := 1839.94 * V1]
+custo_vitima[estado_fisico == "ferido leve",total := 8635.77 * V1]
+custo_vitima[estado_fisico == "ferido grave",total := 141155.96 * V1]
+custo_vitima[estado_fisico == "morto",total := 433286 * V1]
+custo_vitima <- custo_vitima[!is.na(total)]
+(custo_vitima[ano == '2020',]$total/10^6) %>% sum()
+# [1] 545.7242
+(custo_vitima$total/10^9) %>% sum()
+# [1] 9.554445
 # geral
 vitima_all$estado_fisico %>% unique()
 vitima_all[estado_fisico %in% c("ferido leve","ferido grave","morto"),N] %>% sum()
